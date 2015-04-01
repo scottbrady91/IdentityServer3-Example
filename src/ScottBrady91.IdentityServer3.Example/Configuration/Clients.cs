d8f37d34e2b60eb705296eb97036235369deeff1
Scott Brady <scottbrady91@outlook.com>
2015-04-01 19:15:40 +0100

    Solution creation and initial package install

---
+++ src/ScottBrady91.IdentityServer3.Example/Configuration/Clients.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Thinktecture.IdentityServer.Core.Models;
+
+namespace ScottBrady91.IdentityServer3.Example.Configuration
+{
+    public static class Clients
+    {
+        public static IEnumerable<Client> Get()
+        {
+            return new[]
+            {
+                new Client
+                {
+                    ClientId = @"implicitclient", 
+                    ClientName = @"Implicit Client",
+                    Enabled = true,
+                    Flow = Flows.Implicit,
+
+                }
+            };
+        }
+    }
+}
