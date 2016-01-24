0ccb03fad5cf8843f2fffbb4b83f09321ad2d3b5
Scott Brady <scottbrady91@outlook.com>
2016-01-24 22:53:41 +0000

    Added Ws-Federation Example

---
+++ src/ScottBrady91.IdentityServer3.Example/Configuration/RelyingParties.cs
@@ -0,0 +1,37 @@
+﻿namespace ScottBrady91.IdentityServer3.Example.Configuration
+{
+    using System.Collections.Generic;
+    using System.Security.Claims;
+
+    using IdentityModel.Constants;
+
+    using global::IdentityServer3.WsFederation.Models;
+
+    public static class RelyingParties
+    {
+        public static IEnumerable<RelyingParty> Get()
+        {
+            return new List<RelyingParty>
+                       {
+                           new RelyingParty
+                               {
+                                   Realm = "urn:testClient", 
+                                   Name = "testclient", 
+                                   Enabled = true, 
+                                   ReplyUrl = "https://localhost:4004/TestClient/", 
+                                   TokenType = TokenTypes.Saml2TokenProfile11, 
+                                   ClaimMappings =
+                                       new Dictionary<string, string>
+                                           {
+                                               { "sub", ClaimTypes.NameIdentifier }, 
+                                               { "name", ClaimTypes.Name }, 
+                                               { "given_name", ClaimTypes.GivenName }, 
+                                               { "family_name", ClaimTypes.Surname }, 
+                                               { "email", ClaimTypes.Email }, 
+                                               { "upn", ClaimTypes.Upn }
+                                           }
+                               }
+                       };
+        }
+    }
+}
---
+++ src/ScottBrady91.IdentityServer3.Example/Startup_WsFederation.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Owin;
+
+using ScottBrady91.IdentityServer3.Example;
+
+[assembly: OwinStartup("WsFederation", typeof(Startup_WsFederation))]
+
+namespace ScottBrady91.IdentityServer3.Example
+{
+    using System.Collections.Generic;
+
+    using global::IdentityServer3.Core.Configuration;
+    using global::IdentityServer3.WsFederation.Configuration;
+    using global::IdentityServer3.WsFederation.Models;
+    using global::IdentityServer3.WsFederation.Services;
+
+    using Owin;
+
+    using ScottBrady91.IdentityServer3.Example.Configuration;
+
+    public sealed class Startup_WsFederation
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Map(
+                "/core",
+                coreApp =>
+                    {
+                        coreApp.UseIdentityServer(
+                            new IdentityServerOptions
+                                {
+                                    SiteName = "Standalone Identity Server",
+                                    SigningCertificate = Cert.Load(),
+                                    Factory =
+                                        new IdentityServerServiceFactory().UseInMemoryClients(Clients.Get())
+                                        .UseInMemoryScopes(Scopes.Get())
+                                        .UseInMemoryUsers(Users.Get()),
+                                    RequireSsl = true,
+                                    PluginConfiguration = ConfigureWsFederation
+                                });
+                    });
+        }
+
+        private void ConfigureWsFederation(IAppBuilder pluginApp, IdentityServerOptions options)
+        {
+            var factory = new WsFederationServiceFactory(options.Factory);
+            factory.Register(new Registration<IEnumerable<RelyingParty>>(RelyingParties.Get()));
+            factory.RelyingPartyService = new Registration<IRelyingPartyService>(typeof(InMemoryRelyingPartyService));
+            pluginApp.UseWsFederationPlugin(new WsFederationPluginOptions { IdentityServerOptions = options, Factory = factory });
+        }
+    }
+}
